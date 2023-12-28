874ebaddfbb9940c9c746c6d89a4f1f59f4c6a4b
Moa Hedendahl <moahedendahl@gmail.com>
2023-12-28 11:19:16 +0100

    comment out code

--- api/recipe.ts
+++ api/recipe.ts
@@ -1,6 +1,3 @@
-import { doc, setDoc } from 'firebase/firestore';
-import { FIREBASE_DB } from '../FirebaseConfig';
-
 // Function to get the balance from the database
 // export async function getApiBalance() {
 //   try {
@@ -25,23 +22,23 @@ import { FIREBASE_DB } from '../FirebaseConfig';
 // }
 
 // Function to write the daily special to the database
-export async function writeDailySpecial(
-  description: string,
-  price: number,
-  milk: string,
-  vegan: boolean
-) {
-  try {
-    const specialOfTheDay = doc(FIREBASE_DB, 'dailySpecial/2023-12-28');
-    const docData = {
-      description,
-      price,
-      milk,
-      vegan,
-    };
-    await setDoc(specialOfTheDay, docData);
-  } catch (error) {
-    console.error('Error writing daily special:', error);
-    throw error; // Propagate the error to the caller
-  }
-}
+// export async function writeDailySpecial(
+//   description: string,
+//   price: number,
+//   milk: string,
+//   vegan: boolean
+// ) {
+//   try {
+//     const specialOfTheDay = doc(FIREBASE_DB, 'dailySpecial/2023-12-28');
+//     const docData = {
+//       description,
+//       price,
+//       milk,
+//       vegan,
+//     };
+//     await setDoc(specialOfTheDay, docData);
+//   } catch (error) {
+//     console.error('Error writing daily special:', error);
+//     throw error; // Propagate the error to the caller
+//   }
+// }
