6821a9c647bb1e82f7f0627d4ecb3c005d6e648e
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-12-28 12:02:24 +0100

    moved initializeAuth under initializeApp

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -23,9 +23,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
-export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
-export const FIREBASE_DB = getFirestore(FIREBASE_APP);
-
 initializeAuth(FIREBASE_APP, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
+
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const FIREBASE_DB = getFirestore(FIREBASE_APP);
+
