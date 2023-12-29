ad00d44ed0264a25fbfa4b6fda3229bcbb88f284
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-12-29 13:00:24 +0100

    commented out insideLayout, now using RootNavigator instead

--- App.tsx
+++ App.tsx
@@ -6,30 +6,28 @@ import { PaperProvider } from 'react-native-paper';
 import { FIREBASE_AUTH } from './FirebaseConfig';
 import theme from './Theme';
 import RootNavigator from './navigators/RootNavigator';
-import HomeScreen from './screens/HomeScreen';
-import List from './screens/List';
 import SignInScreen from './screens/SignInScreen';
 
 const Stack = createNativeStackNavigator();
-const InsideStack = createNativeStackNavigator();
-function InsideLayout() {
-  return (
-    <InsideStack.Navigator>
-      <InsideStack.Screen
-        name="List"
-        component={List}
-        options={{ headerShown: false }}
-      />
-      <InsideStack.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{ headerShown: false }}
-      />
-      {/* <InsideStack.Screen name="Profile" component={Profile} />
-      <InsideStack.Screen name="Explore" component={Explore} /> */}
-    </InsideStack.Navigator>
-  );
-}
+// const InsideStack = createNativeStackNavigator();
+// function InsideLayout() {
+//   return (
+//     <InsideStack.Navigator>
+//       <InsideStack.Screen
+//         name="List"
+//         component={ListScreen}
+//         options={{ headerShown: false }}
+//       />
+//       <InsideStack.Screen
+//         name="Home"
+//         component={HomeScreen}
+//         options={{ headerShown: false }}
+//       />
+//       {/* <InsideStack.Screen name="Profile" component={Profile} />
+//       <InsideStack.Screen name="Explore" component={Explore} /> */}
+//     </InsideStack.Navigator>
+//   );
+// }
 
 function App() {
   const [user, setUser] = useState<User | null>(null);
