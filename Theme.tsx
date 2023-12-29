31af2e48e518b3620574ee845ccf223dedc0adef
Moa Hedendahl <moahedendahl@gmail.com>
2023-12-29 09:47:52 +0100

    create theme

---
+++ Theme.tsx
@@ -0,0 +1,13 @@
+import { MD3LightTheme } from 'react-native-paper';
+
+const theme = {
+  ...MD3LightTheme,
+  colors: {
+    ...MD3LightTheme.colors,
+    primary: '#232323',
+    secondary: '#D6DED1',
+    background: '#fff',
+  },
+};
+
+export default theme;
