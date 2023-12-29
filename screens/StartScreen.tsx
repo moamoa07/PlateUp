1bb1c89bc76e48ce7da86edca5e0d23941960ce1
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-12-29 14:11:10 +0100

    renmaed start

--- screens/StartScreen.tsx
+++ screens/StartScreen.tsx
@@ -1,7 +1,14 @@
+import { useFonts } from 'expo-font';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-paper';
 
 function Start() {
+  const [fontsLoaded] = useFonts({
+    CrakeRegular: require('../assets/fonts/craketest-regular.otf'),
+    CrakeBold: require('../assets/fonts/craketest-bold.otf'),
+    Jost: require('../assets/fonts/Jost-VariableFont_wght.ttf'),
+  });
+
   return (
     <View style={styles.container}>
       <Text style={styles.h1}>Welcome to PlateUp</Text>
@@ -9,11 +16,15 @@ function Start() {
         Delighted you're joining us! Create an account to save and share your
         unique recipes with our community. Start your culinary journey now!
       </Text>
-      <Button mode="contained" buttonColor="#000000">
-        Create a profile
+      <Button
+        mode="contained"
+        buttonColor="#000000"
+        style={styles.containedButton}
+      >
+        <Text style={styles.textBtn}>Create a profile</Text>
       </Button>
       <Button mode="outlined" textColor="#000000" style={styles.outlinedButton}>
-        Sign in
+        <Text style={styles.textBtn}>Sign in</Text>
       </Button>
     </View>
   );
@@ -32,13 +43,22 @@ const styles = StyleSheet.create({
   h1: {
     fontSize: 40,
     textAlign: 'center',
+    fontFamily: 'CrakeRegular',
   },
   p: {
     fontSize: 15,
     textAlign: 'center',
+    fontFamily: 'Jost',
+  },
+  textBtn: {
+    fontFamily: 'Jost',
+  },
+  containedButton: {
+    borderRadius: 10,
   },
   outlinedButton: {
     borderColor: '#000000',
+    borderRadius: 10,
   },
 });
 
