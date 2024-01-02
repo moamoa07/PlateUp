b96b321de4293fb02923aaedd1114d8a7f93f197
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-01-02 17:32:22 +0100

    changed order

--- Init.ts
+++ Init.ts
@@ -2,10 +2,10 @@ import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 
 export const initializeApp = async () => {
-  await SplashScreen.hideAsync();
   await Font.loadAsync({
     'Crake-Regular': require('./assets/fonts/craketest-regular.otf'),
     'Crake-Bold': require('./assets/fonts/craketest-bold.otf'),
     'Jost-Regular': require('./assets/fonts/Jost-VariableFont_wght.ttf'),
   });
+  await SplashScreen.hideAsync();
 };
