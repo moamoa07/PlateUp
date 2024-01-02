3b5cb5f35f95d33d433afc5359dcb7178c589ae6
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-01-02 17:26:54 +0100

    change names on fonts

--- Init.ts
+++ Init.ts
@@ -4,8 +4,8 @@ import * as SplashScreen from 'expo-splash-screen';
 export const initializeApp = async () => {
   await SplashScreen.hideAsync();
   await Font.loadAsync({
-    CrakeRegular: require('./assets/fonts/craketest-regular.otf'),
-    CrakeBold: require('./assets/fonts/craketest-bold.otf'),
-    Jost: require('./assets/fonts/Jost-VariableFont_wght.ttf'),
+    'Crake-Regular': require('./assets/fonts/craketest-regular.otf'),
+    'Crake-Bold': require('./assets/fonts/craketest-bold.otf'),
+    'Jost-Regular': require('./assets/fonts/Jost-VariableFont_wght.ttf'),
   });
 };
