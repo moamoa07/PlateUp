4545a69dacaf1e130f16852f97fce0010bc5bf87
Moa Hedendahl <moahedendahl@gmail.com>
2024-01-02 11:42:11 +0100

    create screens for user profile navigation to setting and bookmark

---
+++ screens/SettingScreen.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+function SettingScreen() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Setting Screen</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default SettingScreen;
