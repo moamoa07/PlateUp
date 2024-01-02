c109319694b8d5f9992d4d1e7db8e7def2f78f02
Moa Hedendahl <moahedendahl@gmail.com>
2024-01-02 12:46:12 +0100

    start layout for setting screen

--- screens/SettingScreen.tsx
+++ screens/SettingScreen.tsx
@@ -1,14 +1,60 @@
-import { Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button, useTheme } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 function SettingScreen() {
+  const theme = useTheme();
+  const [isLoaded] = useFonts({
+    CrakeRegular: require('../assets/fonts/craketest-regular.otf'),
+    CrakeBold: require('../assets/fonts/craketest-bold.otf'),
+    Jost: require('../assets/fonts/Jost-VariableFont_wght.ttf'),
+  });
+
+  const handleOnLayout = useCallback(async () => {
+    if (isLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [isLoaded]);
+
+  if (!isLoaded) {
+    return null;
+  }
+
   return (
-    <SafeAreaView>
+    <SafeAreaView onLayout={handleOnLayout}>
       <View>
-        <Text>Setting Screen</Text>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Settings</Text>
+        </View>
+        <Button icon="account-edit-outline" mode="outlined">
+          Edit profile
+        </Button>
+        <Button icon="help-circle-outline" mode="outlined">
+          Help
+        </Button>
+        <Button icon="logout" mode="outlined">
+          Sign out
+        </Button>
       </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: 'CrakeBold',
+    fontSize: 24,
+  },
+  titleContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  text: {
+    fontFamily: 'Jost',
+  },
+});
 
 export default SettingScreen;
