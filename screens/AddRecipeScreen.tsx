1fe29f4ed9ad9f210f868aabb014114ceba129d7
lisamarieandersson <lisamarie.andersson@gmail.com>
2024-01-03 15:07:26 +0100

    added recipe form component

--- screens/AddRecipeScreen.tsx
+++ screens/AddRecipeScreen.tsx
@@ -1,10 +1,12 @@
 import { SafeAreaView, Text, View } from 'react-native';
+import AddRecipeForm from '../components/addRecipeForm';
 
 function AddRecipeScreen() {
   return (
     <SafeAreaView>
       <View>
         <Text>Recipe Screen</Text>
+        <AddRecipeForm />
       </View>
     </SafeAreaView>
   );
