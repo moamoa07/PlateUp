06305caf88d985144327e4ba0bedf776f755c141
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-01-04 16:45:18 +0100

    move code from SignIn to CreateProfile

--- screens/CreateProfileScreen.tsx
+++ screens/CreateProfileScreen.tsx
@@ -1,11 +1,161 @@
-import { View, Text } from 'react-native';
+import { Link } from '@react-navigation/native';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, StyleSheet, View } from 'react-native';
+import { Button, Text, TextInput, useTheme } from 'react-native-paper';
+import { FIREBASE_AUTH } from '../FirebaseConfig';
 
 function CreateProfileScreen() {
+  const theme = useTheme();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const auth = FIREBASE_AUTH;
+  const [isFocused, setFocused] = useState(false);
+
+  const createProfile = async () => {
+    try {
+      const response = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      // console.log(response);
+      alert('Check your email!');
+    } catch (error: any) {
+      /*Change to another type instead of any!!*/
+      console.log(error);
+      alert('Registration failed, try again!' + error.message);
+    }
+  };
+
   return (
-    <View>
-      <Text>CreateProfile</Text>
+    <View
+      style={[styles.container, { backgroundColor: theme.colors.background }]}
+    >
+      <KeyboardAvoidingView behavior="padding">
+        <Text style={[styles.title]}>Sign In</Text>
+        <TextInput
+          value={email}
+          label={<Text style={{ fontFamily: 'Jost-Regular' }}>Email</Text>}
+          mode="outlined"
+          autoCapitalize="none"
+          contentStyle={{ fontFamily: 'Jost-Regular' }}
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
+          outlineStyle={{
+            borderRadius: 10,
+            borderColor: isFocused
+              ? theme.colors.primary
+              : theme.colors.secondary,
+          }}
+          style={[styles.textInput, { marginBottom: 10 }]}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+          value={password}
+          label={<Text style={{ fontFamily: 'Jost-Regular' }}>Password</Text>}
+          mode="outlined"
+          autoCapitalize="none"
+          contentStyle={{ fontFamily: 'Jost-Regular' }}
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
+          outlineStyle={{
+            borderRadius: 10,
+            borderColor: isFocused
+              ? theme.colors.primary
+              : theme.colors.secondary,
+          }}
+          style={[styles.textInput]}
+          secureTextEntry={true}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <Text style={[styles.passwordText, { color: theme.colors.primary }]}>
+          Forgot password?
+        </Text>
+        <View style={[styles.buttonContainer]}>
+          <Button mode="contained" onPress={createProfile}>
+            <Text style={styles.textWhite}>Create Profile</Text>
+          </Button>
+        </View>
+        <View style={[styles.linkContainer]}>
+          <Text style={[styles.linkText1]}>Already have a profile?</Text>
+          <Link to={{ screen: 'SignInScreen' }}>
+            <Text style={[styles.linkText2]}>Sign in</Text>
+          </Link>
+        </View>
+        <Text style={[styles.termsText]}>
+          By signing up, you agree to PlateUp's
+        </Text>
+        <Text>Terms of Use & Privacy Policy</Text>
+      </KeyboardAvoidingView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    paddingHorizontal: 10,
+    paddingTop: 70,
+    display: 'flex',
+    alignItems: 'center',
+  },
+  title: {
+    fontFamily: 'Crake-Regular',
+    textAlign: 'center',
+    fontSize: 24,
+    height: 30,
+    marginBottom: 15,
+  },
+  textInput: {
+    width: 260,
+    borderRadius: 25,
+  },
+  passwordText: {
+    fontFamily: 'Jost-Regular',
+    marginLeft: 5,
+    marginTop: 4,
+    marginBottom: 30,
+  },
+  buttonContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: 50,
+  },
+  button: {
+    borderRadius: 10,
+  },
+  textWhite: {
+    fontFamily: 'Jost-Regular',
+    fontSize: 15,
+    color: '#fff',
+  },
+  termsTextContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: 40,
+  },
+  termsText: {
+    color: '#696969',
+  },
+  linkContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    gap: 5,
+  },
+  linkText1: {
+    fontFamily: 'Jost-Regular',
+    fontSize: 16,
+  },
+  linkText2: {
+    fontFamily: 'Jost-Regular',
+    fontSize: 16,
+    textDecorationLine: 'underline',
+    textDecorationStyle: 'solid',
+    textDecorationColor: '#000',
+  },
+});
 
 export default CreateProfileScreen;
