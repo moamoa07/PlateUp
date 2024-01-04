365c43e27609f7a3849ad23df0fac35a2111ce1e
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-01-04 15:30:17 +0100

    create CreateProfileScreen

---
+++ screens/CreateProfileScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+function CreateProfileScreen() {
+  return (
+    <View>
+      <Text>CreateProfile</Text>
+    </View>
+  );
+}
+
+export default CreateProfileScreen;
