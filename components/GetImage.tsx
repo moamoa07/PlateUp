77807079dbb6f1c7b8893b1e63b2f4dca0f58a3f
Moa Hedendahl <moahedendahl@gmail.com>
2024-01-04 14:02:02 +0100

    create component that gets the image and displays it

---
+++ components/GetImage.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import { Image, View } from 'react-native';
+import { ActivityIndicator, Text } from 'react-native-paper';
+import { Images } from '../api/model/imageModel';
+import { getImageById } from '../api/recipeService';
+
+function GetImage() {
+  const [image, setImage] = useState<Images | null>(null);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      const imageData = await getImageById('VNmkuGMeWuhqyGcYmhPp');
+      setImage(imageData as Images);
+      setLoading(false);
+    };
+
+    fetchImage();
+  }, []);
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+
+  if (!image) {
+    return <Text>No image found</Text>;
+  }
+  return (
+    <View>
+      {image.imageUrl && (
+        <Image
+          source={{ uri: image.imageUrl }}
+          style={{ width: 'auto', height: 500 }}
+        />
+      )}
+    </View>
+  );
+}
+
+export default GetImage;
