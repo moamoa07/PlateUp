497dc9c79831530e945ba9f41e32e1df2bc248b4
Moa Hedendahl <moahedendahl@gmail.com>
2024-01-04 14:02:42 +0100

    component that lets you pick an image from your albums

---
+++ components/PickImage.tsx
@@ -0,0 +1,57 @@
+import * as ImagePicker from 'expo-image-picker';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Button, Text } from 'react-native-paper';
+import { addImage } from '../api/recipeService';
+import ImageViewer from './ImageViewer';
+
+function PickImage() {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+
+      // Call addRecipe with the selected image URI
+      await addImage({
+        imageUrl: result.assets[0].uri,
+      });
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button onPress={pickImageAsync} mode="outlined" style={styles.button}>
+        <Text>Add image</Text>
+      </Button>
+      <View style={styles.imageContainer}>
+        <ImageViewer
+          placeholderImageSource={require('../assets/waffle.jpg')}
+          selectedImage={selectedImage}
+        />
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+  },
+  button: {
+    borderRadius: 10,
+    marginTop: 40,
+    padding: 10,
+  },
+  imageContainer: {
+    width: '100%',
+  },
+});
+
+export default PickImage;
