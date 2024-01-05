5da471c79d437d67097e712f8f8b1dd3ce3242d4
Moa Hedendahl <moahedendahl@gmail.com>
2024-01-05 10:31:27 +0100

    removed componets from screens that were there just for testing

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,13 +1,11 @@
 import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import PickImage from '../components/PickImage';
 
 function HomeScreen() {
   return (
     <SafeAreaView>
       <View>
         <Text>Home Screen</Text>
-        <PickImage />
       </View>
     </SafeAreaView>
   );
--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,12 +1,10 @@
 import { SafeAreaView, Text, View } from 'react-native';
-import GetImage from '../components/GetImage';
 
 function SearchScreen() {
   return (
     <SafeAreaView>
       <View>
         <Text>Search Screen</Text>
-        <GetImage />
       </View>
     </SafeAreaView>
   );
