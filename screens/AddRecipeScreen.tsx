cab022fd40ba5d3a7a0d6a0b2e805be0c60f3d53
Moa Hedendahl <moahedendahl@gmail.com>
2024-01-05 10:17:56 +0100

    removed a text

--- screens/AddRecipeScreen.tsx
+++ screens/AddRecipeScreen.tsx
@@ -1,11 +1,10 @@
-import { SafeAreaView, Text, View } from 'react-native';
+import { SafeAreaView, View } from 'react-native';
 import AddRecipeForm from '../components/addRecipeForm';
 
 function AddRecipeScreen() {
   return (
     <SafeAreaView>
       <View>
-        <Text>Recipe Screen</Text>
         <AddRecipeForm />
       </View>
     </SafeAreaView>
