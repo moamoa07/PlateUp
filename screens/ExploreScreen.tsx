7e87fa1aef537618a4b6fba029b4d2717398d435
Moa Hedendahl <moahedendahl@gmail.com>
2024-01-09 10:34:25 +0100

    remove unecessary text

--- screens/ExploreScreen.tsx
+++ screens/ExploreScreen.tsx
@@ -1,11 +1,10 @@
-import { SafeAreaView, Text, View } from 'react-native';
+import { SafeAreaView, View } from 'react-native';
 import RecipeComponent from '../components/recipeComponent';
 
 function ExploreScreen() {
   return (
     <SafeAreaView>
       <View>
-        <Text>Explore Screen</Text>
         <RecipeComponent recipeId="tKWpZramrLoHOQBHAHF5" />
       </View>
     </SafeAreaView>
