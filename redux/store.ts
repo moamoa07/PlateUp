1e93acf06d64a1195f4a46e27832bde1d07e6993
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-01-09 11:07:08 +0100

    add userReducer in store.ts

--- redux/store.ts
+++ redux/store.ts
@@ -1,16 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './users';
+
 // ...
 
 const store = configureStore({
   reducer: {
-    users: usersReducer,
+    user: userReducer,
     // recipes: recipeReducer,
     // comments: commentsReducer,
-  }
-})
+  },
+});
 
 // Typescript
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
