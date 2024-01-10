dff83967ac25f4b1ecf42a933addcfba0a8a7ae6
lisamarieandersson <lisamarie.andersson@gmail.com>
2024-01-10 18:44:33 +0100

    updated PickImageProps interface

--- types/PickImageProps.ts
+++ types/PickImageProps.ts
@@ -1,4 +1,8 @@
 export interface PickImageProps {
   onChange: (imageUrl: string | null) => void;
   resetTrigger: boolean;
+  onResetComplete?: () => void;
+  errorMessage?: string; // Error message for the image
+  maxSizeInMB?: number; // Maximum file size in MB
+  onImageUploadError?: (error: string) => void;
 }
