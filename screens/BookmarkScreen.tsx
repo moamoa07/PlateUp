4961c0417c98b9798e0a2b78671163cde9c5f2a2
Moa Hedendahl <moahedendahl@gmail.com>
2024-01-10 20:16:01 +0100

    add function to get recipes by id, changed way of mapping

--- screens/BookmarkScreen.tsx
+++ screens/BookmarkScreen.tsx
@@ -1,6 +1,7 @@
 import { getAuth } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, View } from 'react-native';
+import { ActivityIndicator, View } from 'react-native';
+import { Text } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Recipe } from '../api/model/recipeModel';
 import {
@@ -9,8 +10,36 @@ import {
 } from '../api/service/recipeService';
 import RecipeComponent from '../components/RecipeComponent';
 
+interface RecipeWithId extends Recipe {
+  id: string;
+}
+
+const fetchRecipesByIds = async (recipeIds: string[]) => {
+  return Promise.all(
+    recipeIds.map(async (id: string) => {
+      try {
+        const recipe = await getRecipeById(id);
+        console.log('Processing recipe with id:', id);
+        if (recipe) {
+          // Only process the recipe if it is not null
+          console.log('Recipe found:', recipe);
+          return recipe;
+        } else {
+          console.log('Recipe not found for ID:', id);
+          return null;
+        }
+      } catch (error) {
+        console.error('Error fetching recipe with ID:', id, error);
+        return null;
+      }
+    })
+  );
+};
+
 function BookmarkScreen() {
-  const [bookmarkedRecipes, setBookmarkedRecipes] = useState<Recipe[]>([]);
+  const [bookmarkedRecipes, setBookmarkedRecipes] = useState<RecipeWithId[]>(
+    []
+  );
   const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -20,36 +49,17 @@ function BookmarkScreen() {
 
       if (user) {
         try {
-          // Specify the type of recipeIds as string[]
           let recipeIds: string[] = await getBookmarkedRecipes(user.uid);
-
-          // Filter out potential null or undefined values
           recipeIds = recipeIds.filter((id) => !!id);
 
           console.log('recipeIds', recipeIds);
 
-          const recipes = await Promise.all(
-            recipeIds.map(async (id: string) => {
-              const recipe = await getRecipeById(id);
-              console.log('Processing recipe with id:', id);
-              if (recipe) {
-                // Only process the recipe if it is not null
-                console.log('Recipe found:', recipe);
-                return recipe;
-              } else {
-                console.log('Recipe not found for ID:', id);
-                return null;
-              }
-            })
-          );
+          const recipes = await fetchRecipesByIds(recipeIds);
 
           console.log('recipes', recipes);
 
           // Filter out null recipes before setting the state
-          setBookmarkedRecipes(
-            recipes.filter((recipe) => recipe !== null) as Recipe[]
-          );
-          console.log('recipes bookmarked', bookmarkedRecipes);
+          setBookmarkedRecipes(recipes as RecipeWithId[]);
           console.log('recipes test', recipes);
         } catch (error) {
           console.error('Error fetching bookmarked recipes:', error);
@@ -78,31 +88,15 @@ function BookmarkScreen() {
 
   return (
     <SafeAreaView>
-      <FlatList
-        data={bookmarkedRecipes}
-        keyExtractor={(item, index) =>
-          item && item.timestamp ? item.timestamp.toString() : index.toString()
-        }
-        renderItem={({ item }) => <RecipeComponent recipeId={item.id} />}
-      />
+      {bookmarkedRecipes.length > 0 ? (
+        bookmarkedRecipes.map((recipe) => (
+          <RecipeComponent key={recipe.id} recipeId={recipe.id} />
+        ))
+      ) : (
+        <Text>No recipes found</Text>
+      )}
     </SafeAreaView>
   );
 }
 
-{
-  /* <SafeAreaView>
-<FlatList
-  data={bookmarkedRecipes}
-  keyExtractor={(item, index) =>
-    item && item.id ? item.id.toString() : index.toString()
-  }
-  renderItem={({ item }) => (
-    <View>
-      <Text>Title: {item.title}</Text>
-      <Text>Timestamp: {item.timestamp.toString()}</Text>
-    </View>
-  )}
-/>
-</SafeAreaView> */
-}
 export default BookmarkScreen;
