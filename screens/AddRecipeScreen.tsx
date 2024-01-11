1f401507e477e766eac772bfbc41ecc43d49d360
lisamarieandersson <lisamarie.andersson@gmail.com>
2024-01-11 16:19:09 +0100

    added temporary headline (might change to real header later)

--- screens/AddRecipeScreen.tsx
+++ screens/AddRecipeScreen.tsx
@@ -1,14 +1,31 @@
-import { SafeAreaView, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import theme from '../Theme';
 import AddRecipeForm from '../components/AddRecipeForm';
 
 function AddRecipeScreen() {
   return (
     <SafeAreaView>
       <View>
+        <View style={styles.screenHeader}>
+          <Text style={styles.h3}>Add New Recipe</Text>
+        </View>
         <AddRecipeForm />
       </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  screenHeader: {
+    marginTop: 32,
+    paddingBottom: 16,
+  },
+  h3: {
+    fontSize: 28,
+    fontFamily: 'Crake-Regular',
+    textAlign: 'center',
+    color: theme.colors.primary,
+  },
+});
 
 export default AddRecipeScreen;
