bd696135368fc6544fbfdb4c8e344143179f88c2
lisamarieandersson <lisamarie.andersson@gmail.com>
2024-01-12 15:59:34 +0100

    commented out headline

--- screens/ExploreScreen.tsx
+++ screens/ExploreScreen.tsx
@@ -1,34 +1,34 @@
-import { PixelRatio, SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import theme from '../Theme';
+import { SafeAreaView, View } from 'react-native';
+import RecipesList from '../components/RecipeList';
 
-const thinBorder = 1 / PixelRatio.get();
+// const thinBorder = 1 / PixelRatio.get();
 
 function ExploreScreen() {
   return (
     <SafeAreaView>
       <View>
-        <View style={styles.screenHeader}>
+        {/* <View style={styles.screenHeader}>
           <Text style={styles.h3}>Explore Recipes</Text>
-        </View>
-        {/* <RecipesList /> */}
+        </View> */}
+        <RecipesList />
       </View>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  screenHeader: {
-    marginTop: 32,
-    paddingBottom: 16,
-    borderBottomWidth: thinBorder,
-    borderBottomColor: theme.colors.primary,
-  },
-  h3: {
-    fontSize: 28,
-    fontFamily: 'Crake-Regular',
-    textAlign: 'center',
-    color: theme.colors.primary,
-  },
-});
+// const styles = StyleSheet.create({
+//   screenHeader: {
+//     marginTop: 32,
+//     paddingBottom: 16,
+//     borderBottomWidth: thinBorder,
+//     borderBottomColor: theme.colors.primary,
+//   },
+//   h3: {
+//     fontSize: 28,
+//     fontFamily: 'Crake-Regular',
+//     textAlign: 'center',
+//     color: theme.colors.primary,
+//   },
+// });
 
 export default ExploreScreen;
