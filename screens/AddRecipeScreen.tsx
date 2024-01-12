d004acf7e3730a4ade94f8d07f85e57b8904731c
lisamarieandersson <lisamarie.andersson@gmail.com>
2024-01-12 13:38:13 +0100

    commented out headline for now, using it inside Add recipe form component instead

--- screens/AddRecipeScreen.tsx
+++ screens/AddRecipeScreen.tsx
@@ -1,31 +1,30 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import theme from '../Theme';
+import { SafeAreaView, View } from 'react-native';
 import AddRecipeForm from '../components/AddRecipeForm';
 
 function AddRecipeScreen() {
   return (
     <SafeAreaView>
       <View>
-        <View style={styles.screenHeader}>
+        {/* <View style={styles.screenHeader}>
           <Text style={styles.h3}>Add New Recipe</Text>
-        </View>
+        </View> */}
         <AddRecipeForm />
       </View>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  screenHeader: {
-    marginTop: 32,
-    paddingBottom: 16,
-  },
-  h3: {
-    fontSize: 28,
-    fontFamily: 'Crake-Regular',
-    textAlign: 'center',
-    color: theme.colors.primary,
-  },
-});
+// const styles = StyleSheet.create({
+//   screenHeader: {
+//     marginTop: 32,
+//     paddingBottom: 16,
+//   },
+//   h3: {
+//     fontSize: 28,
+//     fontFamily: 'Crake-Regular',
+//     textAlign: 'center',
+//     color: theme.colors.primary,
+//   },
+// });
 
 export default AddRecipeScreen;
