d9fa7a1536fa30d24486c5901324932b4a18ecc8
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-01-14 23:10:49 +0100

    fix keyboard and other details on sign in

--- screens/SignInScreen.tsx
+++ screens/SignInScreen.tsx
@@ -1,7 +1,17 @@
 import { Link } from '@react-navigation/native';
-import { User, UserCredential, signInWithEmailAndPassword } from 'firebase/auth';
+import {
+  User,
+  UserCredential,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
 import { useState } from 'react';
-import { KeyboardAvoidingView, StyleSheet, View } from 'react-native';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
 import { Button, Text, TextInput, useTheme } from 'react-native-paper';
 import { FIREBASE_AUTH } from '../FirebaseConfig';
 import { useAppDispatch } from '../hooks/reduxHooks';
@@ -14,20 +24,26 @@ function SignInScreen() {
   const auth = FIREBASE_AUTH;
   const [isFocused, setFocused] = useState(false);
   const dispatch = useAppDispatch();
-  
+
   const signIn = async () => {
     try {
-      const response: UserCredential = await signInWithEmailAndPassword(auth, email, password);
+      const response: UserCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       const user: User | null = response.user;
 
       if (user) {
         // If you want to store the user's information in your Redux store
-        dispatch(setUser({
-          id: user.uid,
-          displayName: user.displayName || '',
-          email: user.email || '',
-          photoURL: user.photoURL || null,
-        }));
+        dispatch(
+          setUser({
+            id: user.uid,
+            displayName: user.displayName || '',
+            email: user.email || '',
+            photoURL: user.photoURL || null,
+          })
+        );
 
         alert('You signed in!');
       } else {
@@ -41,84 +57,94 @@ function SignInScreen() {
   };
 
   return (
-    <View
-      style={[styles.container, { backgroundColor: theme.colors.background }]}
+    <KeyboardAvoidingView
+      behavior="padding"
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.background,
+        paddingTop: 200,
+      }}
     >
-      <KeyboardAvoidingView behavior="padding">
-        <Text style={[styles.title]}>Sign In</Text>
-        <TextInput
-          value={email}
-          label={<Text style={{ fontFamily: 'Jost-Regular' }}>Email</Text>}
-          mode="outlined"
-          autoCapitalize="none"
-          contentStyle={{ fontFamily: 'Jost-Regular' }}
-          onFocus={() => setFocused(true)}
-          onBlur={() => setFocused(false)}
-          outlineStyle={{
-            borderRadius: 10,
-            borderColor: isFocused
-              ? theme.colors.primary
-              : theme.colors.secondary,
-          }}
-          style={[styles.textInput, { marginBottom: 10 }]}
-          onChangeText={(text) => setEmail(text)}
-        />
-        <TextInput
-          value={password}
-          label={<Text style={{ fontFamily: 'Jost-Regular' }}>Password</Text>}
-          mode="outlined"
-          autoCapitalize="none"
-          contentStyle={{ fontFamily: 'Jost-Regular' }}
-          onFocus={() => setFocused(true)}
-          onBlur={() => setFocused(false)}
-          outlineStyle={{
-            borderRadius: 10,
-            borderColor: isFocused
-              ? theme.colors.primary
-              : theme.colors.secondary,
-          }}
-          style={[styles.textInput]}
-          secureTextEntry={true}
-          onChangeText={(text) => setPassword(text)}
-        />
-        <Text style={[styles.passwordText, { color: theme.colors.primary }]}>
-          Forgot password?
-        </Text>
-        <View style={[styles.buttonContainer]}>
-          <Button
-            mode="contained"
-            buttonColor={theme.colors.primary}
-            labelStyle={{ marginVertical: 10 }}
-            style={[styles.button]}
-            onPress={signIn}
-          >
-            <Text style={[styles.textWhite]}>Sign in</Text>
-          </Button>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View
+          style={[
+            styles.container,
+            { backgroundColor: theme.colors.background },
+          ]}
+        >
+          <Text style={[styles.title]}>Sign In</Text>
+          <TextInput
+            value={email}
+            label={<Text style={{ fontFamily: 'Jost-Regular' }}>Email</Text>}
+            mode="outlined"
+            autoCapitalize="none"
+            contentStyle={{ fontFamily: 'Jost-Regular' }}
+            onFocus={() => setFocused(true)}
+            onBlur={() => setFocused(false)}
+            outlineStyle={{
+              borderRadius: 10,
+              borderColor: isFocused
+                ? theme.colors.primary
+                : theme.colors.secondary,
+            }}
+            style={[styles.textInput, { marginBottom: 10 }]}
+            onChangeText={(text) => setEmail(text)}
+          />
+          <TextInput
+            value={password}
+            label={<Text style={{ fontFamily: 'Jost-Regular' }}>Password</Text>}
+            mode="outlined"
+            autoCapitalize="none"
+            contentStyle={{ fontFamily: 'Jost-Regular' }}
+            onFocus={() => setFocused(true)}
+            onBlur={() => setFocused(false)}
+            outlineStyle={{
+              borderRadius: 10,
+              borderColor: isFocused
+                ? theme.colors.primary
+                : theme.colors.secondary,
+            }}
+            style={[styles.textInput]}
+            secureTextEntry={true}
+            onChangeText={(text) => setPassword(text)}
+          />
+          <Text style={[styles.passwordText, { color: theme.colors.primary }]}>
+            Forgot password?
+          </Text>
+          <View style={[styles.buttonContainer]}>
+            <Button
+              mode="contained"
+              buttonColor={theme.colors.primary}
+              labelStyle={{ marginVertical: 10 }}
+              style={[styles.button]}
+              onPress={signIn}
+            >
+              <Text style={[styles.textWhite]}>Sign in</Text>
+            </Button>
+          </View>
+          <View style={[styles.linkContainer]}>
+            <Text style={[styles.linkText1]}>New to PlateUp?</Text>
+            <Link to={{ screen: 'CreateProfileScreen' }}>
+              <Text style={[styles.linkText2]}>Create a profile</Text>
+            </Link>
+          </View>
         </View>
-        <View style={[styles.linkContainer]}>
-          <Text style={[styles.linkText1]}>New to PlateUp?</Text>
-          <Link to={{ screen: 'CreateProfileScreen' }}>
-            <Text style={[styles.linkText2]}>Create a profile</Text>
-          </Link>
-        </View>
-      </KeyboardAvoidingView>
-    </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     width: '100%',
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'center'
   },
   title: {
     fontFamily: 'Crake-Regular',
     textAlign: 'center',
     fontSize: 28,
-    height: 30,
+    height: 36,
     marginBottom: 20,
   },
   textInput: {
