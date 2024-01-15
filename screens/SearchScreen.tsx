e24c9352ec6a1bda71d17e789b47840345ea1863
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-01-15 14:37:32 +0100

    style empty text

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { SafeAreaView, StyleSheet, TouchableOpacity, View } from 'react-native';
 import { Searchbar, Text } from 'react-native-paper';
 
 function SearchScreen() {
@@ -14,8 +14,40 @@ function SearchScreen() {
           value={searchQuery}
           style={[styles.searchbar]}
         />
-        <View>
-          <Text>Discover recipes and connect with others!</Text>
+        {/* Buttons to toggle between Ingredients and Instructions */}
+        <View style={styles.buttonsContainer}>
+          <TouchableOpacity
+          // style={[styles.button, showIngredients && styles.activeButton]}
+          // onPress={() => toggleSection('ingredients')}
+          >
+            <Text
+            // style={[
+            //   styles.buttonText,
+            //   showIngredients && styles.activeButtonText,
+            // ]}
+            >
+              Ingredients
+            </Text>
+          </TouchableOpacity>
+          <Text>|</Text>
+          <TouchableOpacity
+          // style={[styles.button, !showIngredients && styles.activeButton]}
+          // onPress={() => toggleSection('instructions')}
+          >
+            <Text
+            // style={[
+            //   styles.buttonText,
+            //   !showIngredients && styles.activeButtonText,
+            // ]}
+            >
+              Instructions
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={[styles.emptySearchTextContainer]}>
+          <Text style={[styles.emptySearchText]}>
+            Discover recipes and connect with others!
+          </Text>
         </View>
       </View>
     </SafeAreaView>
@@ -27,7 +59,28 @@ const styles = StyleSheet.create({
     display: 'flex',
     alignItems: 'center',
   },
-  searchbar: { borderRadius: 10, width: '90%' },
+  searchbar: {
+    borderRadius: 10,
+    width: '90%',
+  },
+  emptySearchTextContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '80%',
+    paddingHorizontal: 10,
+  },
+  emptySearchText: {
+    fontFamily: 'Crake-Regular',
+    fontSize: 40,
+    textAlign: 'center',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    // justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
 });
 
 export default SearchScreen;
