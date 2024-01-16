9a64edf6fd9538a2a85683e68eca6897e5d564ab
lisamarieandersson <lisamarie.andersson@gmail.com>
2024-01-16 15:54:15 +0100

    updated Action types

--- types/Action.ts
+++ types/Action.ts
@@ -3,12 +3,17 @@ import { Recipe, RecipeWithId } from '../api/model/recipeModel';
 
 export const FETCH_RECIPES = 'FETCH_RECIPES';
 export const SET_LOADING = 'SET_LOADING';
+export const FETCH_RECIPE_START = 'FETCH_RECIPE_START';
+export const FETCH_RECIPE_SUCCESS = 'FETCH_RECIPE_SUCCESS';
+export const FETCH_RECIPE_ERROR = 'FETCH_RECIPE_ERROR';
 
 // Define your RecipeState type
 export interface RecipeState {
   recipes: (Recipe | RecipeWithId)[];
   lastFetchedRecipe: QueryDocumentSnapshot<DocumentData> | null;
   isLoading: boolean;
+  currentRecipe: Recipe | RecipeWithId | null; // Updated to include Recipe or RecipeWithId
+  error: string | null;
 }
 
 export interface FetchRecipesAction {
@@ -24,4 +29,22 @@ export interface SetLoadingAction {
   payload: boolean;
 }
 
-export type RecipeActionTypes = FetchRecipesAction | SetLoadingAction;
+export interface FetchRecipeStartAction {
+  type: typeof FETCH_RECIPE_START;
+}
+
+export interface FetchRecipeSuccessAction {
+  type: typeof FETCH_RECIPE_SUCCESS;
+  payload: Recipe | RecipeWithId;
+}
+
+export interface FetchRecipeErrorAction {
+  type: typeof FETCH_RECIPE_ERROR;
+  payload: string;
+}
+export type RecipeActionTypes =
+  | FetchRecipesAction
+  | SetLoadingAction
+  | FetchRecipeStartAction
+  | FetchRecipeSuccessAction
+  | FetchRecipeErrorAction;
