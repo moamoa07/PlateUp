2ef5c4496547f365fa0b8d3b8dc93ac7c833db68
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-01-17 12:29:46 +0100

    add userService and a function to getAllUsers

---
+++ api/service/userService.ts
@@ -0,0 +1,16 @@
+import { collection, getDocs } from 'firebase/firestore';
+import { FIREBASE_DB } from '../../FirebaseConfig';
+import { CustomUser } from '../model/userModel';
+
+export async function getAllUsers() {
+  const querySnapshot = await getDocs(collection(FIREBASE_DB, 'users'));
+
+  const fetchedUsers = querySnapshot.docs.map((doc) => ({
+    ...(doc.data() as CustomUser),
+    id: doc.id,
+  }));
+
+  return {
+    users: fetchedUsers,
+  };
+}
