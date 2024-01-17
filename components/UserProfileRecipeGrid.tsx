65d732f8aafb272f097f8fee6b239afc8023907e
lisamarieandersson <lisamarie.andersson@gmail.com>
2024-01-17 22:31:57 +0100

    added component UserProfileRecipe grid

--- components/UserProfileRecipeGrid.tsx
+++ components/UserProfileRecipeGrid.tsx
@@ -1,29 +1,36 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { FlatList, Image, StyleSheet, TouchableOpacity } from 'react-native';
-import { useSelector } from 'react-redux';
-import { selectUserRecipes } from '../redux/reducers/recipes';
-import { currentUser } from '../redux/users';
+import { useDispatch, useSelector } from 'react-redux';
+import { RecipeWithId } from '../api/model/recipeModel';
+import { fetchUserRecipes, selectUserRecipes } from '../redux/reducers/recipes';
+import { currentUser } from '../redux/reducers/users';
+import { AppDispatch, RootState } from '../redux/store';
 
-// ... other imports
-
-const UserProfileRecipeGrid = ({ navigation, userId }) => {
-  // State to hold user's recipes
-  const [userRecipes, setUserRecipes] = useState([]);
-  // In your UserProfile component
-  const userId = useSelector(currentUser)?.id; // Assuming currentUser returns the logged-in user
-  const userRecipes = useSelector((state) => selectUserRecipes(state, userId));
+function UserProfileRecipeGrid({ navigation }: { navigation: any }) {
+  const dispatch = useDispatch<AppDispatch>();
+  const userId = useSelector(currentUser)?.id ?? '';
+  const userRecipes = useSelector((state: RootState) =>
+    selectUserRecipes(state, userId)
+  );
 
   useEffect(() => {
-    // Fetch recipes for the given userId
-    // Update setUserRecipes with the fetched data
-  }, [userId]);
+    if (userId) {
+      dispatch(fetchUserRecipes(userId));
+    }
+  }, [userId, dispatch]);
 
-  const renderRecipeThumbnail = ({ item }) => (
+  const renderRecipeThumbnail = ({ item }: { item: RecipeWithId }) => (
     <TouchableOpacity
       onPress={() => navigation.navigate('RecipeDetail', { recipeId: item.id })}
     >
-      <Image source={{ uri: item.imageUrl }} style={styles.thumbnail} />
-      {/* Add other details like title, etc., if needed */}
+      <Image
+        source={{
+          uri:
+            item.imageUrl ??
+            require('../assets/img/add-new-recipe-placeholder.png'),
+        }}
+        style={styles.thumbnail}
+      />
     </TouchableOpacity>
   );
 
@@ -31,15 +38,25 @@ const UserProfileRecipeGrid = ({ navigation, userId }) => {
     <FlatList
       data={userRecipes}
       renderItem={renderRecipeThumbnail}
+      style={styles.gridContainer}
       // Define styles for numColumns and other layout properties
-      numColumns={2}
+      numColumns={3}
       keyExtractor={(item) => item.id}
     />
   );
-};
+}
 
 const styles = StyleSheet.create({
-  thumbnail: {},
+  gridContainer: {
+    marginVertical: 0,
+    display: 'flex',
+    gap: 4,
+  },
+  thumbnail: {
+    width: 120,
+    height: 120,
+    flex: 3,
+  },
 });
 
 export default UserProfileRecipeGrid;
