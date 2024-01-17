0bcb86c82e1b35ff0795bff033b2453c2f57e7ff
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-01-17 12:38:47 +0100

    add reducers to user

--- redux/reducers/users.ts
+++ redux/reducers/users.ts
@@ -5,6 +5,8 @@ import type { RootState } from '../store';
 // Define a type for the slice state
 export interface UserState {
   user: CustomUser | null;
+  loading: boolean;
+  users: CustomUser[]; // You may need an array to store fetched users
 }
 
 // Define the initial state using that type
@@ -15,6 +17,8 @@ const initialState: UserState = {
     displayName: '',
     photoURL: null,
   },
+  loading: false,
+  users:[],
 };
 
 export const userSlice = createSlice({
@@ -28,12 +32,20 @@ export const userSlice = createSlice({
     setSignOutState: (state) => {
       state.user = null;
     },
+    setLoading: (state, action: PayloadAction<boolean>) => {
+      state.loading = action.payload;
+    },
+    setUsers: (state, action: PayloadAction<CustomUser[]>) => {
+      state.users = action.payload;
+    }
   },
 });
 
-export const { setUser, setSignOutState } = userSlice.actions;
+export const { setUser, setSignOutState, setLoading, setUsers } = userSlice.actions;
 
 // Selector to get the user from the state
 export const currentUser = (state: RootState) => state.user.user;
+export const isLoading = (state: RootState) => state.user.loading;
+export const getUsers = (state: RootState) => state.user.users;
 
 export default userSlice.reducer;
