d852725d331eb570f4db8a6746c534c894c8b929
lisamarieandersson <lisamarie.andersson@gmail.com>
2024-01-17 01:20:53 +0100

    added message for when user reaches end of list

--- components/RecipeList.tsx
+++ components/RecipeList.tsx
@@ -4,6 +4,8 @@ import {
   FlatList,
   PixelRatio,
   StyleSheet,
+  Text,
+  View,
 } from 'react-native';
 import theme from '../Theme';
 import { RecipeWithId } from '../api/model/recipeModel';
@@ -43,59 +45,23 @@ const RecipeList = () => {
       data={recipes.filter((recipe): recipe is RecipeWithId => !!recipe.id)}
       renderItem={({ item }) => <RecipeComponent recipe={item} />}
       keyExtractor={(item, index) => item.id ?? index.toString()}
+      ListHeaderComponent={
+        <View style={styles.screenHeader}>
+          <Text style={styles.h3}>Explore Recipes</Text>
+        </View>
+      }
       ListFooterComponent={
-        isLoading ? <ActivityIndicator size={'large'} /> : null
+        isLoading ? (
+          <ActivityIndicator size={'large'} />
+        ) : !hasMoreRecipes ? (
+          <Text style={styles.endOfListMessage}>You've reached the end!</Text>
+        ) : null
       }
       onEndReached={handleLoadMore}
       onEndReachedThreshold={0.5}
     />
   );
 };
-
-//   return (
-//     <ScrollView>
-//       <View style={styles.screenHeader}>
-//         <Text style={styles.h3}>Explore Recipes</Text>
-//       </View>
-
-//       {/* Loader */}
-//       {isLoading && <ActivityIndicator size={'large'} />}
-
-//       {/* Recipes List */}
-//       {!isLoading &&
-//         recipes.map((recipe) => {
-//           // Assert that each recipe is a RecipeWithId
-//           const recipeWithId = recipe as RecipeWithId;
-//           return (
-//             <RecipeComponent key={recipeWithId.id} recipe={recipeWithId} />
-//           );
-//         })}
-
-//       {/* Load More Button */}
-//       {!isLoading && hasMoreRecipes && (
-//         <TouchableOpacity
-//           onPress={handleLoadMore}
-//           style={styles.buttonTouchable}
-//         >
-//           <Button
-//             mode="contained"
-//             style={styles.loadMoreButton}
-//             labelStyle={styles.buttonLabel}
-//           >
-//             Load More Recipes
-//           </Button>
-//         </TouchableOpacity>
-//       )}
-
-//       {/* End of List Message */}
-//       {!isLoading && !hasMoreRecipes && (
-//         <Text style={styles.endOfListMessage}>
-//           You've reached the last recipe.
-//         </Text>
-//       )}
-//     </ScrollView>
-//   );
-// };
 
 const styles = StyleSheet.create({
   screenHeader: {
@@ -109,25 +75,6 @@ const styles = StyleSheet.create({
     fontFamily: 'Crake-Regular',
     textAlign: 'center',
     color: theme.colors.primary,
-  },
-  buttonTouchable: {
-    borderRadius: 10,
-    flex: 1,
-  },
-  loadMoreButton: {
-    fontFamily: 'Jost-Regular',
-    borderRadius: 10,
-    backgroundColor: theme.colors.primary,
-    marginTop: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 2,
-    marginBottom: 20,
-    marginHorizontal: 16,
-  },
-  buttonLabel: {
-    fontFamily: 'Jost-Regular',
-    fontSize: 16,
   },
   endOfListMessage: {
     fontFamily: 'Jost-Regular',
