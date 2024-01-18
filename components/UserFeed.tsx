4814ecb47eba3349c553b5a45e360bf82b487946
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-01-18 13:20:07 +0100

    create userfeed (similar to explore)

--- components/UserFeed.tsx
+++ components/UserFeed.tsx
@@ -1,12 +1,93 @@
-import { View } from 'react-native';
-import { Text } from 'react-native-paper';
+import React, { useEffect } from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  PixelRatio,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import theme from '../Theme';
+import { useAppDispatch, useAppSelector } from '../hooks/reduxHooks';
+import { fetchRecipes } from '../redux/actions/recipeActions';
+import {
+  selectHasMoreRecipes,
+  selectIsLoading,
+  selectLastFetchedRecipeId,
+  selectRecipes,
+} from '../redux/reducers/recipes';
+import RecipeFeed from './RecipeFeed';
 
-function UserFeed() {
+const thinBorder = 1 / PixelRatio.get();
+
+const UserFeed = () => {
+  const dispatch = useAppDispatch();
+  const recipes = useAppSelector(selectRecipes);
+  const lastFetchedRecipeId = useAppSelector(selectLastFetchedRecipeId);
+  const isLoading = useAppSelector(selectIsLoading);
+  const hasMoreRecipes = useAppSelector(selectHasMoreRecipes);
+
+  // For optimizing performance
+  // Prevents unnecessary re-renders of list items when the data hasn't changed
+  const RecipeFeedMemo = React.memo(RecipeFeed);
+
+  useEffect(() => {
+    if (!recipes.length) {
+      dispatch(fetchRecipes(null, 3));
+    }
+  }, [dispatch]);
+
+  const handleLoadMore = () => {
+    if (hasMoreRecipes) {
+      dispatch(fetchRecipes(lastFetchedRecipeId, 3));
+    }
+  };
+
   return (
-    <View>
-      <Text>Userfeed</Text>
-    </View>
+    <FlatList
+      data={recipes}
+      renderItem={({ item }) => <RecipeFeedMemo recipe={item} />}
+      keyExtractor={(item, index) => item.id ?? index.toString()}
+      ListHeaderComponent={
+        <View style={styles.screenHeader}>
+          <Text style={styles.h3}>User Feed</Text>
+        </View>
+      }
+      ListFooterComponent={
+        isLoading ? (
+          <ActivityIndicator size={'large'} />
+        ) : !hasMoreRecipes ? (
+          <Text style={styles.endOfListMessage}>
+            You've reached the last recipe!
+          </Text>
+        ) : null
+      }
+      onEndReached={handleLoadMore}
+      onEndReachedThreshold={0.5}
+    />
   );
-}
+};
+
+const styles = StyleSheet.create({
+  screenHeader: {
+    marginTop: 32,
+    paddingBottom: 16,
+    borderBottomWidth: thinBorder,
+    borderBottomColor: theme.colors.primary,
+  },
+  h3: {
+    fontSize: 28,
+    fontFamily: 'Crake-Regular',
+    textAlign: 'center',
+    color: theme.colors.primary,
+  },
+  endOfListMessage: {
+    fontFamily: 'Jost-Regular',
+    textAlign: 'center',
+    fontSize: 16,
+    marginVertical: 20,
+    color: '#888',
+  },
+});
 
 export default UserFeed;
