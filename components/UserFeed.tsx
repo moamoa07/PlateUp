18a348b43353d68dc9308b6179645c84ca3efd8b
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-01-18 11:58:12 +0100

    create userfeed component

---
+++ components/UserFeed.tsx
@@ -0,0 +1,12 @@
+import { View } from 'react-native';
+import { Text } from 'react-native-paper';
+
+function UserFeed() {
+  return (
+    <View>
+      <Text>Userfeed</Text>
+    </View>
+  );
+}
+
+export default UserFeed;
