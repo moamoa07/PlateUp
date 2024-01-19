5c12c7324efc9e1ac7496f52554db51144ac9b60
Moa Hedendahl <moahedendahl@gmail.com>
2024-01-19 12:52:58 +0100

    add styling and stack for settingscreen

--- navigators/ProfileStack.tsx
+++ navigators/ProfileStack.tsx
@@ -1,13 +1,22 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import ProfileScreen from '../screens/ProfileScreen';
 import RecipeDetailScreen from '../screens/RecipeDetailScreen';
+import SettingScreen from '../screens/SettingScreen';
 import { RootStackParamList } from '../types/RootStackParamList';
 
 const ProfileStack = createStackNavigator<RootStackParamList>();
 
 function ProfileStackNavigator() {
   return (
-    <ProfileStack.Navigator>
+    <ProfileStack.Navigator
+      screenOptions={{
+        headerMode: 'screen',
+        headerTintColor: 'black',
+        headerStyle: { backgroundColor: '#fff' },
+        cardStyle: { backgroundColor: '#fff' },
+        cardShadowEnabled: false,
+      }}
+    >
       <ProfileStack.Screen
         name="UserProfile"
         component={ProfileScreen}
@@ -25,6 +34,18 @@ function ProfileStackNavigator() {
         component={RecipeDetailScreen}
         options={{
           headerTitle: 'Recipe',
+          headerTitleStyle: {
+            color: '#232323',
+            fontFamily: 'Crake-Regular',
+            fontSize: 28,
+          },
+        }}
+      />
+      <ProfileStack.Screen
+        name="Setting"
+        component={SettingScreen}
+        options={{
+          headerTitle: 'Settings',
           headerTitleStyle: {
             color: '#232323',
             fontFamily: 'Crake-Regular',
