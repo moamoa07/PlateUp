95126bfecc261630d0ddc7b386458300f2e163f7
Moa Hedendahl <moahedendahl@gmail.com>
2024-01-19 12:54:04 +0100

    add type

--- types/RootStackParamList.ts
+++ types/RootStackParamList.ts
@@ -1,6 +1,8 @@
 // RootStackParamList.ts
 export type RootStackParamList = {
   UserProfile: undefined;
+  Setting: undefined;
+
   RecipeDetail: { recipeId: string };
   // ... other screens with parameters
 };
