935b2ba989630385434c87b6495dd858af4d8733
lisamarieandersson <lisamarie.andersson@gmail.com>
2024-01-19 10:41:25 +0100

    added options to ProfileStack screen

--- navigators/ProfileStack.tsx
+++ navigators/ProfileStack.tsx
@@ -1,15 +1,29 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import ProfileScreen from '../screens/ProfileScreen';
 import RecipeDetailScreen from '../screens/RecipeDetailScreen';
-import { RootStackParamList } from './RootStackParamList';
+import { RootStackParamList } from '../types/RootStackParamList';
 
 const ProfileStack = createStackNavigator<RootStackParamList>();
 
 function ProfileStackNavigator() {
   return (
     <ProfileStack.Navigator>
-      <ProfileStack.Screen name="UserProfile" component={ProfileScreen} />
-      <ProfileStack.Screen name="RecipeDetail" component={RecipeDetailScreen} />
+      <ProfileStack.Screen
+        name="UserProfile"
+        component={ProfileScreen}
+        options={{
+          headerTitle: 'User Profile',
+          headerTitleStyle: { fontFamily: 'Jost-Regular', fontSize: 20 },
+        }}
+      />
+      <ProfileStack.Screen
+        name="RecipeDetail"
+        component={RecipeDetailScreen}
+        options={{
+          headerTitle: 'Recipe Detail',
+          headerTitleStyle: { fontFamily: 'Jost-Regular', fontSize: 20 },
+        }}
+      />
     </ProfileStack.Navigator>
   );
 }
