125d9e0ebd4b2f697119a5cdec3c62fb8b547da5
Moa Hedendahl <moahedendahl@gmail.com>
2024-01-19 13:24:58 +0100

    add bookmark type

--- types/RootStackParamList.ts
+++ types/RootStackParamList.ts
@@ -2,7 +2,7 @@
 export type RootStackParamList = {
   UserProfile: undefined;
   Setting: undefined;
-
+  Bookmark: undefined;
   RecipeDetail: { recipeId: string };
   // ... other screens with parameters
 };
