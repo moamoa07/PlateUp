3b739d0d9e8b85602ffa0fd012bf03b797c50da6
lisamarieandersson <lisamarie.andersson@gmail.com>
2024-01-23 10:55:17 +0100

    moved functions for bookmarks to their own service file

--- api/service/recipeService.ts
+++ api/service/recipeService.ts
@@ -1,8 +1,6 @@
 import {
   QueryConstraint,
   addDoc,
-  arrayRemove,
-  arrayUnion,
   collection,
   doc,
   getDoc,
@@ -10,14 +8,12 @@ import {
   limit,
   orderBy,
   query,
-  setDoc,
   startAfter,
   updateDoc,
   where,
 } from 'firebase/firestore';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { FIREBASE_DB, FIREBASE_STORAGE } from '../../FirebaseConfig';
-import { UserBookmarks } from '../../types/UserBookmarks';
 import convertFirestoreRecipeToAppRecipe from '../../utils/convertFirestoreRecipeToAppRecipe';
 import { Recipe } from '../model/recipeModel';
 
@@ -211,63 +207,5 @@ export async function uploadImageToFirestore(
   } catch (error) {
     console.error('Error uploading image:', error);
     return null; // Return null in case of error
-  }
-}
-
-// Firestore service functions for bookmarks
-const bookmarksRef = collection(FIREBASE_DB, 'bookmarks');
-
-export async function addBookmarkToFirestore(
-  userId: string,
-  recipeId: string
-): Promise<void> {
-  try {
-    const userBookmarkDocRef = doc(bookmarksRef, userId);
-    await setDoc(
-      userBookmarkDocRef,
-      {
-        recipeIds: arrayUnion(recipeId),
-      },
-      { merge: true }
-    );
-  } catch (error) {
-    console.error('Error adding bookmark:', error);
-    throw error; // Re-throw the error if you want to handle it in the caller
-  }
-}
-
-export async function removeBookmarkFromFirestore(
-  userId: string,
-  recipeId: string
-): Promise<void> {
-  try {
-    const userBookmarkDocRef = doc(bookmarksRef, userId);
-    await updateDoc(userBookmarkDocRef, {
-      recipeIds: arrayRemove(recipeId),
-    });
-  } catch (error) {
-    console.error('Error removing bookmark:', error);
-    throw error;
-  }
-}
-
-export async function getBookmarksFromFirestore(
-  userId: string
-): Promise<string[]> {
-  try {
-    const userBookmarkDocRef = doc(bookmarksRef, userId);
-    const docSnap = await getDoc(userBookmarkDocRef);
-
-    if (docSnap.exists()) {
-      const data = docSnap.data() as UserBookmarks;
-      return data.recipeIds;
-    } else {
-      // Initialize the bookmarks document for new users
-      await setDoc(userBookmarkDocRef, { recipeIds: [] });
-      return [];
-    }
-  } catch (error) {
-    console.error('Error fetching bookmarks:', error);
-    throw error;
   }
 }
