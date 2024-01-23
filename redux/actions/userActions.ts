f25e16c71d811ed01eba211ddf26303a92b7c76d
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-01-23 23:16:40 +0100

    add fetchLoggedInUser

--- redux/actions/userActions.ts
+++ redux/actions/userActions.ts
@@ -1,6 +1,28 @@
-import { getAllUsers } from '../../api/service/userService';
-import { setLoading, setUsers } from '../reducers/users';
+import { getAllUsers, getLoggedInUser } from '../../api/service/userService';
+import {
+  getUserProfile,
+  setLoading,
+  setUser,
+  setUserProfile,
+  setUsers,
+} from '../reducers/users';
 import { AppDispatch } from '../store';
+
+// Async action to fetch the logged-in user
+export const fetchLoggedInUser = () => async (dispatch: AppDispatch) => {
+  dispatch(setLoading(true)); // Set loading state
+  try {
+    // Fetch the logged-in user using your API function
+    const loggedInUser = await getLoggedInUser();
+
+    // Dispatch success action to update the state
+    dispatch(setUser(loggedInUser));
+  } catch (error) {
+    console.error('Error fetching logged-in user:', error);
+  } finally {
+    dispatch(setLoading(false)); // Reset loading state
+  }
+};
 
 // Async action to fetch users
 export const fetchUsers = () => async (dispatch: AppDispatch) => {
@@ -12,8 +34,23 @@ export const fetchUsers = () => async (dispatch: AppDispatch) => {
     // Dispatch success action to update the state
     dispatch(setUsers(fetchedData.users));
   } catch (error) {
-    console.log(error)
+    console.log(error);
   } finally {
     dispatch(setLoading(false)); // Reset loading state
   }
 };
+
+// export const fetchUserProfile =
+//   (userId: string) => async (dispatch: AppDispatch) => {
+//     try {
+//       dispatch(setLoading(true));
+
+//       const userProfile = getUserProfile(userId);
+
+//       dispatch(setUserProfile(userProfile));
+//     } catch (error) {
+//       console.error('Error fetching user profile:', error);
+//     } finally {
+//       dispatch(setLoading(false));
+//     }
+//   };
