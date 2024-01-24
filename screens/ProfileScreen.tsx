f8dcaa19d7a6c1db946ce415d4d55fe1923e067c
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2024-01-24 17:16:59 +0100

    prettier

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { View } from 'react-native';
 import UserProfileRecipeGrid from '../components/UserProfileRecipeGrid';
 
-function ProfileScreen({ navigation, route }: { navigation: any, route:any }) {
+function ProfileScreen({ navigation, route }: { navigation: any; route: any }) {
   return (
     <View style={{ flex: 1, backgroundColor: '#fff' }}>
       <UserProfileRecipeGrid route={route} navigation={navigation} />
