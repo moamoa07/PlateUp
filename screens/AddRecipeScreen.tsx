f06421214b0acc30c35971176cf5b1568752c5b9
lisamarieandersson <lisamarie.andersson@gmail.com>
2024-01-26 01:48:30 +0100

    removed uncommented code

--- screens/AddRecipeScreen.tsx
+++ screens/AddRecipeScreen.tsx
@@ -5,26 +5,10 @@ function AddRecipeScreen() {
   return (
     <SafeAreaView>
       <View>
-        {/* <View style={styles.screenHeader}>
-          <Text style={styles.h3}>Add New Recipe</Text>
-        </View> */}
         <AddRecipeForm />
       </View>
     </SafeAreaView>
   );
 }
-
-// const styles = StyleSheet.create({
-//   screenHeader: {
-//     marginTop: 32,
-//     paddingBottom: 16,
-//   },
-//   h3: {
-//     fontSize: 28,
-//     fontFamily: 'Crake-Regular',
-//     textAlign: 'center',
-//     color: theme.colors.primary,
-//   },
-// });
 
 export default AddRecipeScreen;
