486e63948fa0e182a56d4ac1f1b2507920782de6
lisamarieandersson <lisamarie.andersson@gmail.com>
2024-01-26 15:55:34 +0100

    updated to our backup database

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -13,24 +13,24 @@ import { getStorage } from 'firebase/storage';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: 'AIzaSyBeNIY00IjPVYGiq8uPbAlPMGqfs7tpvqU',
-  authDomain: 'plateup-1507b.firebaseapp.com',
-  projectId: 'plateup-1507b',
-  storageBucket: 'plateup-1507b.appspot.com',
-  messagingSenderId: '725439259612',
-  appId: '1:725439259612:web:58f9c9d7a291909fed2519',
-};
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyBeNIY00IjPVYGiq8uPbAlPMGqfs7tpvqU',
+//   authDomain: 'plateup-1507b.firebaseapp.com',
+//   projectId: 'plateup-1507b',
+//   storageBucket: 'plateup-1507b.appspot.com',
+//   messagingSenderId: '725439259612',
+//   appId: '1:725439259612:web:58f9c9d7a291909fed2519',
+// };
 
 // Backup database
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyBkRty0aVrJFkursrSbfmbWtbGBok09rqE',
-//   authDomain: 'plateup2.firebaseapp.com',
-//   projectId: 'plateup2',
-//   storageBucket: 'plateup2.appspot.com',
-//   messagingSenderId: '52300252680',
-//   appId: '1:52300252680:web:8e6964e9cb01374812b3bd',
-// };
+const firebaseConfig = {
+  apiKey: 'AIzaSyBkRty0aVrJFkursrSbfmbWtbGBok09rqE',
+  authDomain: 'plateup2.firebaseapp.com',
+  projectId: 'plateup2',
+  storageBucket: 'plateup2.appspot.com',
+  messagingSenderId: '52300252680',
+  appId: '1:52300252680:web:8e6964e9cb01374812b3bd',
+};
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
